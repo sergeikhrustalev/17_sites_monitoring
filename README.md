c627708250de26435cb4b0e26d6de25c7b88b13b
sergei <sergei@Mac-mini-sergei.local>
2017-10-20 00:39:02 +0300

    Ïnitial commit

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+check_sites_health.py - checking sites http status and domain expiration information
+
+
+# Requirements
+This scripts require python3.5+  
+See requirements.txt for a dependencies requirements.
+
+# How to run
+
+```bash
+$python check_sites_health.py urls.txt
+Checking for a site http://google.com
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://instagram.com
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://yandex.ru
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://facebook.com
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://mail.ru/
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://vk.com
+     <OK>	HTTP Status 200
+     <OK>	Domain expiration date more then a month
+Checking for a site http://devman.org/qwerty
+<WARNING>	HTTP Status is not 200
+     <OK>	Domain expiration date more then a month
+```
+
+File urls.txt must contains site urls divided by a new line
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,62 @@
+import sys
+import requests
+import whois
+from datetime import datetime
+
+
 def load_urls4check(path):
-    pass
+
+    with open(path) as url_file:
+        urls_from_file = url_file.readlines()
+
+    return [url_list.strip() for url_list in urls_from_file]
+
 
 def is_server_respond_with_200(url):
-    pass
+
+    status_ok = 200
+
+    if requests.get(url).status_code == status_ok:
+        return True
+
+    return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+
+    exp_date = whois.whois(domain_name).expiration_date
+
+    if isinstance(exp_date, list):
+        exp_date = exp_date[0]
+
+    return exp_date
+
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) == 1:
+        sys.exit('Syntax: python check_sites_health.py <urls.txt>')
+
+    days_in_month = 30
+
+    url_list = load_urls4check(sys.argv[1])
+
+    for url in url_list:
+
+        print('Checking for a site {}'.format(url))
+
+        if is_server_respond_with_200(url):
+            print('     <OK>\tHTTP Status 200')
+        else:
+            print('<WARNING>\tHTTP Status is not 200')
+
+        domain_name = url.split('//')[-1].split('/')[0]
+
+        days_to_expire = (
+            get_domain_expiration_date(domain_name) - datetime.today()
+            ).days
+
+        if days_to_expire > days_in_month:
+            print('     <OK>\tDomain expiration date more then a month')
+        else:
+            print('<WARNING>\tDomain expiration date less then a month')
